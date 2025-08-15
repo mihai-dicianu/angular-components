649693e74d84bcb275ea9dca8c23dc3bbfe724d8
Mihai Dicianu <mihaidicianu@gmail.com>
2025-08-15 23:20:51 +0300

    Add progress indicator controls

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-components');
+  progress = signal(0);
+  radius = signal(50);
+  color = signal('#007bff');
 }
