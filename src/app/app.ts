cfffac9571f4a31828c064bba71b39e42c5c118f
Mihai Dicianu <mihaidicianu@gmail.com>
2025-08-16 08:38:28 +0300

    Add tprogress to app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { TProgress } from './tprogress/tprogress';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, TProgress],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
